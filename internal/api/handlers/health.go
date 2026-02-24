094cd89abeaf85f2383fb8a67cb2c7f9275844d8
reza <reza.sanjari@avl.com>
2026-02-24 08:04:22 +0100

    chore: removed swagger setup

--- internal/api/handlers/health.go
+++ internal/api/handlers/health.go
@@ -6,14 +6,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// Health godoc
-// @Summary      Health check
-// @Description  Returns application health status
-// @Tags         health
-// @Accept       json
-// @Produce      json
-// @Success      200  {object}  map[string]string
-// @Router       /api/health [get]
 func (h *Handler) Health(c echo.Context) error {
 	data, err := h.Services.Health.GetStatus()
 	if err != nil {
