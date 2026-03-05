7a844b60450047c264c4fd378fc624f128adea01
reza <reza.sanjari@avl.com>
2026-03-05 10:29:24 +0100

    added room and websocketEnvelop structs to hub

--- internal/hub/hub.go
+++ internal/hub/hub.go
@@ -1,8 +1,23 @@
 package hub
 
-import "2L1nk/internal/session"
+import (
+	"2L1nk/internal/models"
+	"2L1nk/internal/session"
+	"encoding/json"
+)
 
 type Hub struct{ s *session.Store }
+
+type WSMessageEnvelope struct {
+	Type    models.WSEventType `json:"type"`
+	payload json.RawMessage
+}
+
+type room struct {
+	roomID string
+	users  map[string]*User
+	epoch  int64
+}
 
 func New(s *session.Store) *Hub {
 	return &Hub{s: s}
