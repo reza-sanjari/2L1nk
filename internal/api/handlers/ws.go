1019a48e868a4e6d43a5caaac92fbb98697b9cf9
reza <reza.sanjari@avl.com>
2026-03-10 15:11:38 +0100

    feat: moved to gorilla websocket implementation

--- internal/api/handlers/ws.go
+++ internal/api/handlers/ws.go
@@ -3,11 +3,12 @@ package handlers
 import (
 	"2L1nk/internal/hub"
 	"encoding/json"
-	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
-	"golang.org/x/net/websocket"
+	"go.uber.org/zap"
 )
 
 type AuthPayload struct {
@@ -16,51 +17,71 @@ type AuthPayload struct {
 	Signature string `json:"Chat-Signature"`
 }
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func (h *Handler) Ws(c echo.Context) error {
-	websocket.Handler(func(ws *websocket.Conn) {
-		defer func() {
-			if err := ws.Close(); err != nil {
-				log.Printf("error closing websocket: %v", err)
-			}
-		}()
-
-		// 1. read first message
-		var raw []byte
-		if err := websocket.Message.Receive(ws, &raw); err != nil {
-			log.Println("failed to receive first message:", err)
-			return
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Printf("websocket upgrade failed: %v", err)
+		return err
+	}
+	defer func() {
+		if err := ws.Close(); err != nil {
+			log.Printf("error closing websocket: %v", err)
 		}
+	}()
 
-		// 2. decode envelope
-		var msg hub.WSMessageEnvelope
-		if err := json.Unmarshal(raw, &msg); err != nil {
-			log.Println("invalid message:", err)
-			return
-		}
+	h.Logg.Debug("websocket connection opened")
+	if err := ws.WriteMessage(websocket.TextMessage, []byte(`{"type":"info","payload":{"message":"initiated"}}`)); err != nil {
+		log.Printf("failed to send initiated message: %v", err)
+	}
 
-		// 3. require auth as first message
-		if msg.Type != "auth" {
-			log.Println("first message must be auth")
-			return
-		}
+	h.Logg.Debug("waiting for first websocket message")
 
-		// 4. decode auth payload
-		var auth AuthPayload
-		if err := json.Unmarshal(msg.Payload, &auth); err != nil {
-			log.Println("invalid auth payload:", err)
-			return
-		}
+	// 1. read first message
+	_, raw, err := ws.ReadMessage()
+	if err != nil {
+		log.Println("failed to receive first message:", err)
+		return nil
+	}
 
-		// 5. validate auth
-		activeUser, ok := h.Session.Get(auth.SessionID)
-		if !ok {
-			log.Println("user not active")
-			return
-		}
+	h.Logg.Debug("raw websocket message received", zap.ByteString("raw", raw))
 
-		fmt.Printf("user %v is active", activeUser.Username)
-		// TODO: validate timestamp + signature here
-	}).ServeHTTP(c.Response(), c.Request())
+	// 2. decode envelope
+	var msg hub.WSMessageEnvelope
+	if err := json.Unmarshal(raw, &msg); err != nil {
+		log.Println("invalid message:", err)
+		return nil
+	}
+
+	h.Logg.Debug("received message", zap.Any("msg", msg))
+
+	// 3. require auth as first message
+	if msg.Type != "auth" {
+		log.Println("first message must be auth")
+		return nil
+	}
+
+	// 4. decode auth payload
+	var auth AuthPayload
+	if err := json.Unmarshal(msg.Payload, &auth); err != nil {
+		log.Println("invalid auth payload:", err)
+		return nil
+	}
+
+	// 5. validate auth
+	activeUser, ok := h.Session.Get(auth.SessionID)
+	if !ok {
+		log.Println("user not active")
+		return nil
+	}
+
+	h.Logg.Debug("active user authenticated through websocket", zap.String("username", activeUser.Username))
+	// TODO: validate timestamp + signature here
 
 	return nil
 }
