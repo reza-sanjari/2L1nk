483a580f5dff2f7009b2695d2a425291020e2fbc
reza <reza.sanjari@avl.com>
2026-03-11 10:25:49 +0100

    fix: started hub runner in a goroutine

--- internal/app/app.go
+++ internal/app/app.go
@@ -56,6 +56,7 @@ func New(cfg *config.Config) *App {
 
 	// Hub
 	mainHub := hub.New(sessionStore)
+	go mainHub.Run()
 
 	// Handler
 	handler := handlers.NewHandler(services, mainHub, sessionStore, logg)
