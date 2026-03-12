268189f81fcd1a35a45028eba79800b316fc71fa
reza <reza.sanjari@avl.com>
2026-03-12 09:01:53 +0100

    added websocket message payload strcut

---
+++ internal/hub/payloads.go
@@ -0,0 +1,20 @@
+package hub
+
+import (
+	"2L1nk/internal/models"
+	"encoding/json"
+)
+
+type WSMessageEnvelope struct {
+	Sender  *User              `json:"-"` // server-only
+	Type    models.WSEventType `json:"type"`
+	Payload json.RawMessage    `json:"payload"`
+}
+
+// MessagePayload todo: chance ciphertext type to []byte
+type MessagePayload struct {
+	RoomID     string `json:"room_id"`
+	SenderFP   string `json:"sender_fp"`
+	Epoch      uint64 `json:"epoch"`
+	Ciphertext string `json:"ciphertext"`
+}
