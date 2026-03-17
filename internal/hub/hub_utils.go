37cbf121a60c9a0bc93e2738f2273967ec6eb24d
reza <reza.sanjari@avl.com>
2026-03-17 09:00:46 +0100

    feat: new endpoint for getting all users rooms

--- internal/hub/hub_utils.go
+++ internal/hub/hub_utils.go
@@ -1,9 +1,25 @@
 package hub
 
+import "2L1nk/internal/models"
+
 type UserStatus struct {
 	Username    string `json:"username"`
 	Fingerprint string `json:"fingerprint"`
 	Online      bool   `json:"online"`
+}
+
+type RoomMemberInfo struct {
+	Username    string          `json:"username"`
+	Fingerprint string          `json:"fingerprint"`
+	Mode        models.UserMode `json:"mode"`
+}
+
+type UserRoomInfo struct {
+	RoomID string           `json:"room_id"`
+	Name   string           `json:"name"`
+	Host   RoomMemberInfo   `json:"host"`
+	Users  []RoomMemberInfo `json:"users"`
+	Epoch  int64            `json:"epoch"`
 }
 
 func (h *Hub) getUser(fingerprint string) *User {
@@ -37,4 +53,37 @@ func (h *Hub) GetUsers() []UserStatus {
 	}
 
 	return users
+}
+
+func (h *Hub) GetUserRooms(userFingerprint string) []UserRoomInfo {
+	var rooms []UserRoomInfo
+
+	for _, room := range h.Rooms {
+		if _, ok := room.Users[userFingerprint]; !ok {
+			continue
+		}
+
+		var users []RoomMemberInfo
+		for _, user := range room.Users {
+			users = append(users, RoomMemberInfo{
+				Username:    user.Username,
+				Fingerprint: user.Fingerprint,
+				Mode:        user.Mode,
+			})
+		}
+
+		rooms = append(rooms, UserRoomInfo{
+			RoomID: room.RoomID,
+			Name:   room.Name,
+			Host: RoomMemberInfo{
+				Username:    room.Host.Username,
+				Fingerprint: room.Host.Fingerprint,
+				Mode:        room.Host.Mode,
+			},
+			Users: users,
+			Epoch: room.Epoch,
+		})
+	}
+
+	return rooms
 }
